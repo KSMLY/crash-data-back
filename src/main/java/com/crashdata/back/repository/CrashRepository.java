f20ea84944ae49c2e518c7384890bfd4c85d4a9f
KSMLY <kmsmaily152@gmail.com>
2026-08-24 01:18:18 +0300

    CD-11: convert crash entities to POJOs and remove JPA repositories

--- src/main/java/com/crashdata/back/repository/CrashRepository.java
+++
@@ -1,7 +0,0 @@
-package com.crashdata.back.repository;
-
-import com.crashdata.back.entity.Crash;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CrashRepository extends JpaRepository<Crash, Long> {
-}
