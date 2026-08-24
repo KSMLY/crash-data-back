4b4eed2949725e8df9bb7ced8badcebebc539c9d
KSMLY <kmsmaily152@gmail.com>
2026-08-24 12:10:06 +0300

    CD-13: add POST /crashes

--- src/main/java/com/crashdata/back/service/CrashService.java
+++ src/main/java/com/crashdata/back/service/CrashService.java
@@ -3,6 +3,7 @@ package com.crashdata.back.service;
 import com.crashdata.back.dao.CrashDao;
 import com.crashdata.back.entity.Crash;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
@@ -24,5 +25,9 @@ public class CrashService {
         return crashDao.findById(crashId);
     }
 
-
+    @Transactional
+    public Crash createCrash(Crash crash) {
+        Long id = crashDao.insert(crash);
+        return crashDao.findById(id).orElseThrow();
+    }
 }
