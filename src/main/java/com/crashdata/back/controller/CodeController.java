3d93350ca00b3c9f3bcc4a703494f6b74538301b
KSMLY <kmsmaily152@gmail.com>
2026-09-06 18:18:34 +0300

    CD-23: drop unused name from /codes

--- src/main/java/com/crashdata/back/controller/CodeController.java
+++ src/main/java/com/crashdata/back/controller/CodeController.java
@@ -1,7 +1,6 @@
 package com.crashdata.back.controller;
 
 import com.crashdata.back.code.*;
-import com.crashdata.back.dto.CodeDto;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,47 +11,47 @@ import java.util.Map;
 @RestController
 public class CodeController {
 
-    private static final Map<String, List<CodeDto>> CODES = Map.ofEntries(
-            Map.entry("alcoholSuspected", toDtos(AlcoholSuspected.values())),
-            Map.entry("crashSeverity", toDtos(CrashSeverity.values())),
-            Map.entry("crashType", toDtos(CrashType.values())),
-            Map.entry("curve", toDtos(Curve.values())),
-            Map.entry("drugUse", toDtos(DrugUse.values())),
-            Map.entry("functionalClass", toDtos(FunctionalClass.values())),
-            Map.entry("grade", toDtos(Grade.values())),
-            Map.entry("helmet", toDtos(Helmet.values())),
-            Map.entry("impactType", toDtos(ImpactType.values())),
-            Map.entry("injurySeverity", toDtos(InjurySeverity.values())),
-            Map.entry("junctionType", toDtos(JunctionType.values())),
-            Map.entry("licenceStatus", toDtos(LicenceStatus.values())),
-            Map.entry("light", toDtos(Light.values())),
-            Map.entry("manoeuvre", toDtos(Manoeuvre.values())),
-            Map.entry("obstaclePresent", toDtos(ObstaclePresent.values())),
-            Map.entry("pedManoeuvre", toDtos(PedManoeuvre.values())),
-            Map.entry("restraint", toDtos(Restraint.values())),
-            Map.entry("resultStatus", toDtos(ResultStatus.values())),
-            Map.entry("roadUserType", toDtos(RoadUserType.values())),
-            Map.entry("roadwayType", toDtos(RoadwayType.values())),
-            Map.entry("seatPosition", toDtos(SeatPosition.values())),
-            Map.entry("seatRow", toDtos(SeatRow.values())),
-            Map.entry("sex", toDtos(Sex.values())),
-            Map.entry("specialFunction", toDtos(SpecialFunction.values())),
-            Map.entry("surfaceCondition", toDtos(SurfaceCondition.values())),
-            Map.entry("testStatus", toDtos(TestStatus.values())),
-            Map.entry("testType", toDtos(TestType.values())),
-            Map.entry("trafficControl", toDtos(TrafficControl.values())),
-            Map.entry("vehicleType", toDtos(VehicleType.values())),
-            Map.entry("weather", toDtos(Weather.values()))
+    private static final Map<String, List<Short>> CODES = Map.ofEntries(
+            Map.entry("alcoholSuspected", codesOf(AlcoholSuspected.values())),
+            Map.entry("crashSeverity", codesOf(CrashSeverity.values())),
+            Map.entry("crashType", codesOf(CrashType.values())),
+            Map.entry("curve", codesOf(Curve.values())),
+            Map.entry("drugUse", codesOf(DrugUse.values())),
+            Map.entry("functionalClass", codesOf(FunctionalClass.values())),
+            Map.entry("grade", codesOf(Grade.values())),
+            Map.entry("helmet", codesOf(Helmet.values())),
+            Map.entry("impactType", codesOf(ImpactType.values())),
+            Map.entry("injurySeverity", codesOf(InjurySeverity.values())),
+            Map.entry("junctionType", codesOf(JunctionType.values())),
+            Map.entry("licenceStatus", codesOf(LicenceStatus.values())),
+            Map.entry("light", codesOf(Light.values())),
+            Map.entry("manoeuvre", codesOf(Manoeuvre.values())),
+            Map.entry("obstaclePresent", codesOf(ObstaclePresent.values())),
+            Map.entry("pedManoeuvre", codesOf(PedManoeuvre.values())),
+            Map.entry("restraint", codesOf(Restraint.values())),
+            Map.entry("resultStatus", codesOf(ResultStatus.values())),
+            Map.entry("roadUserType", codesOf(RoadUserType.values())),
+            Map.entry("roadwayType", codesOf(RoadwayType.values())),
+            Map.entry("seatPosition", codesOf(SeatPosition.values())),
+            Map.entry("seatRow", codesOf(SeatRow.values())),
+            Map.entry("sex", codesOf(Sex.values())),
+            Map.entry("specialFunction", codesOf(SpecialFunction.values())),
+            Map.entry("surfaceCondition", codesOf(SurfaceCondition.values())),
+            Map.entry("testStatus", codesOf(TestStatus.values())),
+            Map.entry("testType", codesOf(TestType.values())),
+            Map.entry("trafficControl", codesOf(TrafficControl.values())),
+            Map.entry("vehicleType", codesOf(VehicleType.values())),
+            Map.entry("weather", codesOf(Weather.values()))
     );
 
     @GetMapping("/codes")
-    public Map<String, List<CodeDto>> getCodes() {
+    public Map<String, List<Short>> getCodes() {
         return CODES;
     }
 
-    private static <E extends Enum<E> & CodedEnum> List<CodeDto> toDtos(E[] values) {
+    private static <E extends Enum<E> & CodedEnum> List<Short> codesOf(E[] values) {
         return Arrays.stream(values)
-                .map(v -> new CodeDto(v.getCode(), v.name()))
+                .map(CodedEnum::getCode)
                 .toList();
     }
 }
