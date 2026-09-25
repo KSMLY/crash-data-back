aa6c907fa02608d098829b060961e8f824cb45b7
KSMLY <kmsmaily152@gmail.com>
2026-09-25 06:15:36 +0300

    CD-47: add police ref and crash date to crash points

--- src/main/resources/sql/crash/points.sql
+++ src/main/resources/sql/crash/points.sql
@@ -1,4 +1,4 @@
-SELECT id, latitude, longitude, severity_code
+SELECT id, police_ref, crash_date, latitude, longitude, severity_code
 FROM crash
 WHERE crash_date >= :from_date AND crash_date <= :to_date
 AND latitude IS NOT NULL AND longitude IS NOT NULL
